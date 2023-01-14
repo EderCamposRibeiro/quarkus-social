5103fb30e5a952f9d06c62379700eae1c711da7a
Eder Campos Ribeiro <edercribeiro@gmail.com>
2023-01-14 11:45:02 -0300

    25.Evitando código boilerplate com o Lombok project

--- src/main/java/io/github/edercribeiro/dto/CreateUserRequest.java
+++ src/main/java/io/github/edercribeiro/dto/CreateUserRequest.java
@@ -2,10 +2,13 @@ package io.github.edercribeiro.dto;
 
 // Esse código é o responsável por Respeitar o contrato da API:
 
+import lombok.Data;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.Objects;
 
+@Data
 public class CreateUserRequest {
 
     @NotBlank(message = "Name is Required")
@@ -14,32 +17,4 @@ public class CreateUserRequest {
     @NotNull(message = "Age is Required")
     private Integer age;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CreateUserRequest that = (CreateUserRequest) o;
-        return Objects.equals(name, that.name) && Objects.equals(age, that.age);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, age);
-    }
 }
