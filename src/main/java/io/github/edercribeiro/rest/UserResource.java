ff7a85187334174cbf187e67a9191491bc866513
Eder Campos Ribeiro <edercribeiro@gmail.com>
2023-01-14 11:30:21 -0300

    24.Alterando os códigos de status para mais específicos

--- src/main/java/io/github/edercribeiro/rest/UserResource.java
+++ src/main/java/io/github/edercribeiro/rest/UserResource.java
@@ -35,8 +35,9 @@ public class UserResource {
 
         Set<ConstraintViolation<CreateUserRequest>> violations = validator.validate(userRequest);
         if (!violations.isEmpty()) {
-            ResponseError responseError = ResponseError.createFromValidation(violations);
-            return Response.status(400).entity(responseError).build();
+            return ResponseError
+                    .createFromValidation(violations)
+                    .withStatusCode(ResponseError.UNPROCESSABLE_ENTITY_STATUS);
         }
 
         User user = new User();
@@ -45,7 +46,10 @@ public class UserResource {
 
         repository.persist(user);
 
-        return Response.ok(user).build();
+        return Response
+                .status(Response.Status.CREATED.getStatusCode())
+                .entity(user)
+                .build();
     }
 
     @GET
@@ -62,7 +66,7 @@ public class UserResource {
 
         if (user != null){
             repository.delete(user);
-            return Response.ok().build();
+            return Response.noContent().build();
         }
 
         return Response.status(Response.Status.NOT_FOUND).build();
@@ -76,7 +80,7 @@ public class UserResource {
         if(user != null){
             user.setName(userData.getName());
             user.setAge(userData.getAge());
-            return Response.ok().build();
+            return Response.noContent().build();
         }
         return Response.status(Response.Status.NOT_FOUND).build();
     }
