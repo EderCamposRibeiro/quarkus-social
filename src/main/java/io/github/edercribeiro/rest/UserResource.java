50b41de3d0c2798d4a26412ba1b0c18edd2f7dc9
Eder Campos Ribeiro <edercribeiro@gmail.com>
2023-01-13 22:09:41 -0300

    15.Recebendo e devolvendo objetos JSON

--- src/main/java/io/github/edercribeiro/rest/UserResource.java
+++ src/main/java/io/github/edercribeiro/rest/UserResource.java
@@ -2,15 +2,24 @@ package io.github.edercribeiro.rest;
 
 import io.github.edercribeiro.dto.CreateUserRequest;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
     @POST
     public Response createUser(CreateUserRequest userRequest){
+        return Response.ok(userRequest).build();
+    }
+
+    public Response listAllUsers(){
         return Response.ok().build();
     }
 }
